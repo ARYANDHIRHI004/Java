03258091e5277f95cd92c8822ae7b9c511a2a5e0
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-03-03 18:58:07 +0530

    Operators, Class and objects

---
+++ ObjectsAndMethods/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    int a;
+    String name;
+    public static void main(String[] args) {
+        Main obj = new Main();
+        System.out.println(obj.a);
+        System.out.println(obj.name);
+    }
+}
---
+++ Operators/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        //Arithmetic operators
+        int num1 = 876;
+        int num2 = 6354;
+
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println((float)num1/num2);
+        System.out.println(num1 % num2);
+
+        //Increment and Decrement Operator
+        int a = 5;
+        a++;
+        System.out.println(a);
+        a--;
+        System.out.println(a);
+
+    }
+}
