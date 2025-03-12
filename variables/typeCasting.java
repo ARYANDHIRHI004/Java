c7b7231af50eb70719540205f3955b2d43c877e5
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-03-12 10:24:44 +0530

    type casting

---
+++ variables/typeCasting.java
@@ -0,0 +1,20 @@
+public class typeCasting {
+    public static void main(String[] args) {
+        int a = 10;
+        
+        float b = a;
+        System.out.println(b);
+
+        float c = 12.5f;
+        int d = (int)c;
+        System.out.println(d);
+
+        int n1 = 10;
+        int n2 = 3;
+
+        float res = n1/n2;
+        System.out.println(res);
+        
+        
+    }
+}
