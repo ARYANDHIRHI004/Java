a89b418d67841f26b591f7b682359c6394aea7b6
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-03-12 11:45:00 +0530

    pattern programming

---
+++ loops/pattermProgramming.java
@@ -0,0 +1,24 @@
+public class pattermProgramming {
+    public static void main(String[] args) {
+        for (int j = 0; j < 5; j++) {
+            for (int i = 0; i < 5; i++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        System.out.println("------------------------------------");
+        
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i ==0 || j==0 || i==4 || j==4 ) {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println("");
+        }
+
+    }
+}
