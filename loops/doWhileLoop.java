35e82b7afea41b8e54da1764b06b4fd9516d9a0c
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-03-12 11:28:01 +0530

    loops

---
+++ loops/doWhileLoop.java
@@ -0,0 +1,10 @@
+public class doWhileLoop {
+    public static void main(String[] args) {
+        int n = 10;
+        int i = 1;
+        do { 
+            System.out.print("* ");
+            i++;
+        } while (i<n);
+    }
+}
