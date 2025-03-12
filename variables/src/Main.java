405153d060bc360b09e9283b9a2160507e741867
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-03-12 10:15:08 +0530

    Variables

--- variables/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int age = 18;
-        Datatypes obj = new Datatypes();
-        System.out.println(obj.a);
-
-    }
-}
---
+++ variables/variables.java
@@ -0,0 +1,30 @@
+
+public class variables {
+    public static void main(String[] args) {
+        byte a = 15;
+        System.out.println(a);
+        
+        short b = 553;
+        System.out.println(b);
+
+        int c = 63565;
+        System.out.println(c);
+        
+        long d = 34643134646464456l;
+        System.out.println(d);
+
+        float e = 45.2f;
+        System.out.println(e);
+            
+        double f =  1223.6466;
+        System.out.println(f);
+
+        char g = 'a';
+        System.out.println(g);
+
+        boolean data = true;
+        System.out.println(data);
+
+        
+    }
+}
